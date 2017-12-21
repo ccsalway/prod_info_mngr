7143575c56e9050d9691301ec840fa2994db8e8d
ccsalway <ccsalway@yahoo.co.uk>
2017-12-21 12:34:10 +0000

    Added UTF8 support for MySQL

--- src/main/java/hello/repositories/ProductRepository.java
+++ src/main/java/hello/repositories/ProductRepository.java
@@ -3,8 +3,6 @@ package hello.repositories;
 import hello.entities.Product;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
-
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
     Product findById(Long id);
